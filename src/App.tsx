a968a94ce176d2132dfaad0110659a0957ad0578
zhangliu <liu@liudeMacBook-Air.local>
2026-06-06 22:15:52 +0800

    feat: 实现艺术培训机构-学员档案与班级调整

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+import StudentList from './pages/StudentList';
+import StudentDetail from './pages/StudentDetail';
+import TransferApplication from './pages/TransferApplication';
+import TransferApproval from './pages/TransferApproval';
+import Layout from './components/Layout';
+
+function App() {
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Navigate to="/students" replace />} />
+        <Route path="/students" element={<StudentList />} />
+        <Route path="/students/:id" element={<StudentDetail />} />
+        <Route path="/transfer/new/:studentId" element={<TransferApplication />} />
+        <Route path="/transfer/:id" element={<TransferApplication />} />
+        <Route path="/approvals" element={<TransferApproval />} />
+      </Routes>
+    </Layout>
+  );
+}
+
+export default App;
